b042c0fae3d6f638829fe7fa2df60a0857330273
hugoqdesh <gitjohan5@gmail.com>
2025-09-04 12:36:55 +0300

    better login page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,22 @@
-import SignIn from "@/components/auth/sign-in";
+import { SigninForm } from "@/components/auth/signin-form";
 
-export default function Signin() {
+export default function LoginPage() {
 	return (
-		<main className="flex min-h-screen flex-col gap-28 pt-64 max-w-7xl mx-auto">
-			<SignIn />
-		</main>
+		<div className="grid min-h-svh lg:grid-cols-2">
+			<div className="bg-muted relative hidden lg:block">
+				<img
+					src="/feature.jpg"
+					alt="Image"
+					className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+				/>
+			</div>
+			<div className="flex flex-col gap-4 p-6 md:p-10">
+				<div className="flex flex-1 items-center justify-center">
+					<div className="w-full max-w-xs">
+						<SigninForm />
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
